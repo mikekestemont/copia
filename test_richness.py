9b9ae8e0b06eb25a78c4a88275e30a4c762e3ff4
mikekestemont <mike.kestemont@gmail.com>
2021-01-29 16:38:57 +0100

    add min sample test

--- test_richness.py
+++ test_richness.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 import copia.richness as diversity
+import copia.utils as u
 
 
 def test_chao1():
@@ -49,3 +50,36 @@ def test_iChao4_f4():
     x = np.array([1, 1, 1, 6, 3, 3, 5, 5, 5, 5, 8])
     with pytest.warns(UserWarning):
         diversity.iChao1(x)
+
+
+def test_minsample():
+    # example from the appendix to the original paper:
+    # ref: https://figshare.com/articles/dataset/Supplement_1_Excel-sheet_calculator_and_calculator_instructions_/3530930?backTo=/collections/Sufficient_sampling_for_asymptotic_minimum_species_richness_estimators/3300935
+    # Characteristics of the tested assemblage:
+    # f1 = 3, f2 = 2, n = 161, S_obs = 9, S_est = 11.250, x* = 2.221115367
+    # m = nx* = 357.600 (for g=1)
+
+    # mimic an assemblage with these properties:
+    assemblage = ['A', 'B', 'C']
+    assemblage += ['D'] * 2 + ['E'] * 2
+    assemblage += ['F'] * 3 + ['G'] * 3 + ['H'] * 3
+    assemblage += ['I'] * (161 - len(assemblage))
+    x = u.to_abundance(assemblage)
+
+    # check whether the assemblage is correctly constructed:
+    counts = u.basic_stats(x)
+    assert counts['f1'] == 3
+    assert counts['f2'] == 2
+    assert counts['S'] == 9
+    assert counts['n'] == 161
+
+    d = diversity.diversity(x, 'minsample', solver='grid', diagnostics=True)
+    assert np.isclose(d['x*'], 2.221115367, rtol=0.01)
+    assert np.isclose(d['richness'], 161 + 357.600, rtol=1)
+
+    d = diversity.diversity(x, 'minsample', solver='fsolve', diagnostics=True)
+    assert np.isclose(d['x*'], 2.221115367, rtol=0.01)
+    assert np.isclose(d['richness'], 161 + 357.600, rtol=1)
+    
+
+
