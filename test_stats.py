475648ab05a21c92aba19e85200915b6db34ce31
mikekestemont <mike.kestemont@gmail.com>
2021-01-30 20:09:34 +0100

    test bootstrap seed (2)

--- test_stats.py
+++ test_stats.py
@@ -4,7 +4,6 @@ import warnings
 import numpy as np
 import copia.richness as diversity
 import copia.stats
-import copia.utils as u
 
 
 def test_bootstrap_seed():
@@ -26,6 +25,16 @@ def test_bootstrap_seed():
               n_iter=1000, conf=0.95, n_jobs=4,
               seed=54656)
 
-    assert sorted(a['bootstrap']) == sorted(b['bootstrap'])
-    assert sorted(c['bootstrap']) == sorted(d['bootstrap'])
-    assert sorted(a['bootstrap']) != sorted(d['bootstrap'])
+    assert np.isclose(sorted(a['bootstrap']), sorted(b['bootstrap']),
+                      rtol=0.001).all()
+    assert np.isclose(a['richness'], b['richness'], rtol=0.001)
+    assert np.isclose(a['uci'], b['uci'], rtol=0.001)
+    assert np.isclose(a['lci'], b['lci'], rtol=0.001)
+    assert np.isclose(a['std'], b['std'], rtol=0.001)
+
+    assert np.isclose(sorted(c['bootstrap']), sorted(d['bootstrap']),
+                      rtol=0.001).all()
+    assert np.isclose(c['richness'], d['richness'], rtol=0.001)
+    assert np.isclose(c['uci'], d['uci'], rtol=0.001)
+    assert np.isclose(c['lci'], d['lci'], rtol=0.001)
+    assert np.isclose(c['std'], d['std'], rtol=0.001)
