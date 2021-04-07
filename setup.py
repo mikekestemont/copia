2c2563a30ae43255ed663fda30e964fd5b9943a7
Folgert Karsdorp <fbkarsdorp@gmail.com>
2021-04-07 14:15:39 +0200

    fix versions

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='copia',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/mikekestemont/copia',
     license='CC-BY',
     author='Mike Kestemont & Folgert Karsdorp',
     description='Bias correction for richness in abundance data',
-    requires=['numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'tqdm', 'pytest']
+    install_requires=[
+        'numpy>=1.19.0',
+        'scipy>=1.5.1',
+        'matplotlib>=3.3.2',
+        'seaborn>=0.11.0',
+        'pandas>=1.1.2',
+        'tqdm>=4.48.0',
+        'pytest>=6.2.2'
+    ]
 )
